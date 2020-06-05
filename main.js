9516c5c02bbb46489969335dddc329df6ef5f47d
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-06-05 15:55:21 +0300

    в две строки

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 let num = 266219;
-
 console.log(num.toString().split('').reduce((mult, curr) => mult * curr));
 console.log((num.toString().split('').reduce((a, b) => a * b ) **3).toString().slice(0, 2));
