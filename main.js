b179c187bc73130906afdc3edd6c3194eb546d5f
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-06-10 14:08:24 +0300

    первое задание через фильтр

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 let arr = ['435', '44356', '4363', '52362', '2638547', '2', '6736'];
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i].substring(0, 1) == 4 || arr[i].substring(0, 1) == 2){
-        console.log(+arr[i]);
-    }
-}
+let arr2 = arr.filter(a => a[0] == 4 || a[0] == 2);
+console.log(arr2);
+
 console.log('Простые числа: ');
 let del = 0;
 
@@ -19,7 +17,5 @@ function isPrime (n){
 for(let i = 2; i<=100; i++){
     if(isPrime(i)){
         console.log(i + '  Делители: 1 и ' + i);
-        
     }
 }
-
