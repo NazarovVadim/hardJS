2217fd7bc1ae655f817eaab967b7e543545c13ef
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-06-12 19:10:51 +0300

    поправка

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 const div = document.querySelector('.container');
 let date = new Date();
 
-
 for (let i = 0; i < week.length; i++){
     if(date.getDay() === i+1){
-        if(i>=5){
+        if(week[i].toLowerCase()==='суббота' || week[i].toLowerCase()==='воскресенье'){
             div.innerHTML += `<span><strong><i>${week[i]}</i></strong></span> <br>`;
         } else{
             div.innerHTML += `<span><strong>${week[i]}</strong></span> <br>`;
         }
     } else{
-        if(i>=5){
+        if(week[i].toLowerCase()==='суббота' || week[i].toLowerCase()==='воскресенье'){
             div.innerHTML += `<span><i>${week[i]}</i></span> <br>`;
         } else{
             div.innerHTML += `<span>${week[i]}</span> <br>`;
